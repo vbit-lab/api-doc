8463e2f2a280a7e72463fe8f74878cea6c765f10
vbit-lab <64247868+vbit-lab@users.noreply.github.com>
2020-09-23 20:24:06 +0800

    add official for py

---
+++ official-ws/python/consts.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+# VBIT 生产环境
+VBIT_PROD_WEBSITE_URL = "https://www.vbit.one"
+VBIT_PROD_MARKET_WEBSOCKET_URL = "wss://cdn01.mcdzcloud.com/v1/market"
+VBIT_PROD_MARKET_REST_URL = "https://cdn01.mcdzcloud.com/v1/rest"
+VBIT_PROD_TRADE_WEBSOCKET_URL = "wss://cdn01.mcdzcloud.com/v1/trade"
+VBIT_PROD_TRADE_REST_URL = "https://cdn01.mcdzcloud.com/v1/rest"
+
